193b3422eb5b159037600cd2ce35d645bd389e2a
cotuvideo <cotuvideo@gmail.com>
2017-02-07 15:04:06 +0900

    動画のダウンロードと動画リストの表示

---
+++ smile/index.php
@@ -0,0 +1,50 @@
+<?PHP
+echo "<html>\n";
+
+echo "<head>\n";
+echo "<title>smile</title>\n";
+echo "</head>\n";
+
+echo "<body>\n";
+
+$mysqli = new mysqli("localhost", "", "", "nico");
+if($mysqli->connect_errno)
+{
+	die($mysqli->error);
+}
+$mysqli->query("SET NAMES utf8") or die($mysqli->error);
+
+echo "<table border=\"1\">\n";
+
+$files = scandir('./video/');
+foreach($files as $file)
+{
+	$pos = strpos($file, '.');
+	if($pos !== false && $pos > 0)
+	{
+		$id = substr($file, 0, $pos);
+		echo "<tr>\n";
+		echo "<td>$id</td>\n";
+		$query = "SELECT title,last_visit_date FROM moz_places WHERE video_id='$id'";
+		$result = $mysqli->query($query) or die($mysqli->error."\n");
+		if($row = $result->fetch_assoc())
+		{
+			echo "<td><a href=\"video/$file\" target=\"_blank\">".$row["title"]."</a></td>\n";
+			echo "<td>".date("y-m-d\nH:i:s", $row["last_visit_date"])."</td>\n";
+		}
+		else
+		{
+			echo "<td><a href=\"video/$file\" target=\"_blank\">".$id."</a></td>\n";
+			echo "<td></td>\n";
+		}
+		echo "</tr>\n";
+	}
+}
+$result->free();
+$mysqli->close();
+echo "</table>\n";
+
+echo "</body>\n";
+
+echo "</html>\n";
+?>

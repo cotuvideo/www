879ade6bf963bf73ed753ade47fd82923a00820d
cotuvideo <cotuvideo@gmail.com>
2020-01-09 17:38:45 +0900

    node.js nvm 追加

--- setting/ubuntu/node.js
+++ setting/ubuntu/node.js
@@ -8,3 +8,11 @@ nodebrew install-binary v*.*.*
 nodebrew ls
 nodebrew use v*.*.*
 
+==== nvm ====
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+source ~/.bashrc
+nvm install v10.14.2
+nvm use v10.14.2
+
+curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.13.0
+source ~/.bashrc
